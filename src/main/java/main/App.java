b74b6325f0949b6731d5d24a56f0c5a923bd42e5
CodeIsNotEvil <codeisnotevil@gmail.com>
2020-08-22 18:23:06 +0200

    Added basic maven project with Selenium depenency

---
+++ src/main/java/main/App.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * A utility wich create a folder at ~/Repos 
+ * with the name passed as the first commandline argument.
+ * It also creates a git repository and put it on github.
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // TODO
+    }
+}
