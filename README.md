3d5bf04966deae083324442beaf95475c1fd6077
CodeIsNotEvil <codeisnotevil@gmail.com>
2020-08-27 14:55:44 +0200

    Create a porject on github, the windows folder, add and commit the files

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # Automaticly create a Project Folder to start on.
-A utility wich create a folder at ~/Repos with the name passed as the first commandline argument. It also creates a git repository and put it on github.
+A utility wich create a folder at ~/repos (documents/repos windows) with the name passed as the first commandline argument. It also creates a git repository and put it on github.
 
 #### This should going through the following steps:
-1.  [ ] Create a Folder at ~/Repos with the first argument as it's name.
+1.  [ ] Create a Folder at ~/repos (documents/repos for windows) with the first argument as it's name.
 2.  [x] Create a README&#46;md file inside the folder.
 3.  [ ] Check for user&#46;name and user&#46;email (for now  Only existens) 
 4.  [ ] Initializes a git reposetory inside the folder.
-5.  [ ] Stage the all files.
-6.  [ ] Commit. \(commit -m "init commit"\)
+5.  [x] Stage the README file.
+6.  [x] Commit. \(commit -m "init commit"\)
 7.  [ ] Check if a SSH key is present "ls \-al ~/\.ssh" (looking if there are files other then knwon hosts)
 
 ###### This will be followed if there are no SSH key files:
-8.  [ ] Create on github \(See extra steps\)
+8.  [x] Create on github \(See extra steps\)
 9.  [ ] Add the remote \(git remote add origin https://github.com/CodeIsNotEvil/CreateProjectFolder.git\)
 10. [ ] Push it. \(git push -u origin master\)
 11. [ ] Login. \(to finalize the push action\)
@@ -19,7 +19,7 @@ A utility wich create a folder at ~/Repos with the name passed as the first comm
 
 ###### This will be followed if there are SSH key files:
 8.  [ ] Copy the fingerprint
-9.  [ ] Create on github \(See extra steps\)
+9.  [x] Create on github \(See extra steps\)
 10. [ ] Go to Settings and Select "SSH and PGP" Keys.
 11. [ ] Look for a matching key fingerprint. \(need more details\) 
 12. [ ] If there is a Matching Key close the browser. 
@@ -32,5 +32,5 @@ A utility wich create a folder at ~/Repos with the name passed as the first comm
 9.  [x] Open a Browser. \(Firefox gets implemented and tested first then Chrome\)
 10. [x] Go to github.
 11. [x] Login.
-12. [ ] Create a new public project.
-13. [ ] name it like the Folder.
+12. [x] Create a new public project.
+13. [x] name it like the Folder.
--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,31 +1,52 @@
 package main;
 
 import java.io.File;
+
 import org.apache.commons.exec.OS;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
- * A utility wich create a folder at ~/Repos 
- * with the name passed as the first commandline argument.
- * It also creates a git repository and put it on github.
+ * A utility wich create a folder at ~/Repos with the name passed as the first
+ * commandline argument. It also creates a git repository and put it on github.
  *
  */
-public class App 
-{
+public class App {
     public static void main(String[] args) {
         String folderPath = getOsSpecificFolderPath();
-        createFolder(folderPath, "test" /*args[1]*/); //TODO change if finished
-        //TODO check git user and git init
+        createFolder(folderPath, "test" /* args[1] */); // TODO change if finished
+        // TODO check git user
+        initGitInsideFolder(folderPath + "test" /* args[1] */);
+        
 
         WebDriver driver = new FirefoxDriver();
-        loginToGithub(driver);
+        loginToGithub(driver, "test");
         //driver.close(); //TODO use if everything browser related is done
     }
 
     /**
+     * Initilizes a git repository and commit the README file.
+     * 
+     * @param path fullpath to the folder.
+     */
+    private static void initGitInsideFolder(String path) {
+        try {
+            Git git = Git.init().setDirectory(new File(path)).call();
+            git.add().addFilepattern("README.md").call();
+            git.commit().setMessage("Initial commit").call();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
      * Creates and returns the Os Specific Path.
+     * 
      * @return the Os Specific Path at wich the folder gets created
      */
     public static String getOsSpecificFolderPath(){
@@ -55,30 +76,47 @@ public class App
         try {
             file.mkdir();
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
 
         //Create the README.md file
         try {
             new File(path + name + "/README.md").createNewFile();
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
         
     }
     
     /**
      * Login to the Github.com Website at the webriver.
+     * 
      * @param driver the driver wich is used
+     * @throws InterruptedException
      */
-    public static void loginToGithub(WebDriver driver){
+    public static void loginToGithub(WebDriver driver, String repositoryName) {
         //Open github login page
         driver.get("https://github.com/login");
         //Write login name into field
-        driver.findElement(By.id("login_field")).sendKeys("username");
+        driver.findElement(By.id("login_field")).sendKeys("codeisnotevil");
         //Wirte password into field
-        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.id("password")).sendKeys("z4T9Zolj9e!uj^8C");
         //Submit login form
         driver.findElement(By.name("commit")).click();
+
+        driver.get("https://github.com/new");
+        driver.findElement(By.id("repository_name")).sendKeys(repositoryName);
+
+        //before the button can get pressed wait for 3sec to recognize that the name is accepted
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //System.out.println("\n\nElement: " + driver.findElements(By.xpath("//button[@class=\"btn btn-primary first-in-line\"]")).size() + "\n\n");
+        driver.findElement(By.xpath("//button[@class=\"btn btn-primary first-in-line\"]")).click();
+        //driver.findElement(By.xpath("/html/body/div/main/div/form/div/button")).click();
+
+        
     }
 }
